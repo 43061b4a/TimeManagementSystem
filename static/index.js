5d7df9c7e6392eb501353e71b82b8c519315c88d
AR <1001943+43061b4a@users.noreply.github.com>
2020-08-29 18:15:56 -0600

    Added login and register functionality

--- static/index.js
+++ static/index.js
@@ -1,21 +1,26 @@
 //URLS:
 const AUTH_URL = "/api/api-token-auth/"
-const REGISTER_URL = "/api/api-token-auth/"
+const REGISTER_URL = "/api/register/"
 
 const AUTH_REQUEST = "AUTH_REQUEST";
 const AUTH_SUCCESS = "AUTH_SUCCESS";
 const AUTH_ERROR = "AUTH_ERROR";
 const AUTH_LOGOUT = "AUTH_LOGOUT";
+const AUTH_REGISTER = "AUTH_REGISTER";
+const AUTH_REGISTER_SUCCESS = "AUTH_REGISTER_SUCCESS";
+const AUTH_REGISTER_ERROR = "AUTH_REGISTER_ERROR";
 
 // store
 const store = new Vuex.Store({
     state: {
         token: localStorage.getItem('user-token') || '',
         status: '',
+        registered: '',
     },
     getters: {
         isAuthenticated: state => !!state.token,
         authStatus: state => state.status,
+        registerStatus: state => state.registered,
     },
     actions: {
         [AUTH_REQUEST]: ({commit, dispatch}, user) => {
@@ -39,10 +44,26 @@ const store = new Vuex.Store({
             return new Promise(resolve => {
                 commit(AUTH_LOGOUT);
                 localStorage.removeItem("user-token");
-                console.log(localStorage.getItem('user-token'))
                 resolve();
             });
-        }
+        },
+        [AUTH_REGISTER]: ({commit, dispatch}, user) => {
+            user["profile"] = {"preferred_working_hours": parseInt(user["preferred_working_hours"])}
+            delete user["preferred_working_hours"]
+            console.log(user);
+            return new Promise((resolve, reject) => {
+                commit(AUTH_REGISTER);
+                axios.post(REGISTER_URL, user)
+                    .then(resp => {
+                        commit(AUTH_REGISTER_SUCCESS, resp);
+                        resolve(resp);
+                    })
+                    .catch(err => {
+                        commit(AUTH_REGISTER_ERROR, err);
+                        reject(err);
+                    });
+            });
+        },
     },
     mutations: {
         [AUTH_REQUEST]: state => {
@@ -59,7 +80,18 @@ const store = new Vuex.Store({
         },
         [AUTH_LOGOUT]: state => {
             state.token = "";
-        }
+        },
+        [AUTH_REGISTER]: state => {
+            state.registered = "registering";
+        },
+        [AUTH_REGISTER_ERROR]: (state, err) => {
+            state.registered = "error";
+            state.registration_errors = err;
+        },
+        [AUTH_REGISTER_SUCCESS]: (state, resp) => {
+            state.registered = "registered";
+            state.registration_errors = "";
+        },
     }
 })
 
@@ -72,15 +104,48 @@ const Login = Vue.component('login', {
         };
     },
     template: '#login-template',
+    computed: {
+        isAuthError() {
+            return this.$store.getters.authStatus === 'error'
+        }
+    },
     methods: {
         login: function () {
             const {username, password} = this
-            console.log(username, password);
-            this.$store.dispatch('AUTH_REQUEST', {username, password}).then(() => {
+            this.$store.dispatch(AUTH_REQUEST, {username, password}).then(() => {
                 this.$router.push('/')
-            })
+            }).catch(err => {
+                console.log(err.response.data)
+            });
         }
     }
+});
+
+const Register = Vue.component('register', {
+    data() {
+        return {
+            username: "",
+            password: "",
+            email: "",
+            preferred_working_hours: "",
+        };
+    },
+    template: '#register-template',
+    methods: {
+        register: function () {
+            const {email, username, password, preferred_working_hours} = this
+            this.$store.dispatch(AUTH_REGISTER, {email, username, password, preferred_working_hours}).then(() => {
+                this.$router.push('/register_status')
+            }).catch(err => {
+                console.log(err.response.data)
+            });
+        }
+    },
+    computed: {
+        registerError() {
+            return this.$store.getters.registerStatus === 'error'
+        }
+    },
 });
 const Logout = Vue.component('logout', {
     data() {
@@ -96,13 +161,17 @@ const Logout = Vue.component('logout', {
         this.logout()
     }
 });
-const Register = Vue.component('register', {
-    data: function () {
-        return {}
+const RegisterStatus = Vue.component('register-status', {
+    data() {
+        return {};
     },
-    template: '#register-template',
+    template: '#register-status-template',
+    computed: {
+        isRegistered() {
+            return this.$store.getters.registerStatus === 'registered'
+        }
+    },
 });
-
 
 // router
 const routes = [
@@ -121,6 +190,11 @@ const routes = [
         name: 'logout',
         component: Logout
     },
+    {
+        path: '/register_status',
+        name: 'register_status',
+        component: RegisterStatus
+    }
 ]
 
 const router = new VueRouter({
